c5fdf4379f9979e0aef8d7bcf6654bc82db0ff5b
ITNikolov <ivaylonikolov.n@gmail.com>
2025-07-03 20:04:50 +0300

    ItemConfig creation with name maxQuantity MinSellPrice and MaxSellPrice to get some data from the jsonfile items

---
+++ Models/ItemConfig.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArkRoxBot.Models
+{
+    internal class ItemConfig
+    {
+
+        public string Name { get; set; }
+        public int MaxQuantity { get; set; }
+        public decimal MinSellPrice { get; set; }
+        public decimal MinBuyPrice { get; set; }
+
+    }
+}
